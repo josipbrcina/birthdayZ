e8739cfe1548ffc5c9c3cef73a8ea12c39c64a8d
Josip Brčina <josip@trive.digital>
2018-09-18 14:58:56 +0200

    Include jquery, jquery datepicker and add basic styling

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/jquery', express.static(path.join(__dirname, "/node_modules/jquery/dist/")));
+app.use('/jquery-datepicker', express.static(path.join(__dirname, "/node_modules/jquery-datepicker/")));
 
-app.post('/hola', (req, res) => res.send(`Hello ${req.body.name}`));
+app.post('/get-difference', (req, res) => res.send(`Well done!`));
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('Available at http://localhost:3000'));
